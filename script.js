5561ea47205b9e07921d5adef3dfc1cdc3c16750
ssouron <stef@vertcitron.fr>
2018-12-02 18:18:26 +0100

    ajout d'un composant vue.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+Vue.component('TodoItem', {
+  template: `
+  <div>
+    <span>{{task.text}}</span>
+    <button class="done">Fait</button>
+    <button class="delete disable">Effacer</button>
+  </div>
+  `,
+  props: {
+    task: { type: Object, required: true }
+  }
+})
+
 const app = new Vue({
   el: '#app',
   data: {
